009cc4e6aad72dadceca4bcdc3a320d658a13c56
stranger <aadilgulzari24@gmail.com>
2026-05-10 23:51:20 +0800

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -1,5 +1,5 @@
 --This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.
---Vape rewrite fixed by relevant, the features added are skidded by stranger
+--Vape rewrite fixed by relevant, the features added are skidded by stranger mostly from aerov4
 local run = function(func)
 	func()
 end
