0ae09250316b30da2e959cdf66b7055a5eb2862b
stranger <aadilgulzari24@gmail.com>
2026-05-16 07:25:19 +0800

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -9589,24 +9589,3 @@ run(function()
         end
     })
 end)
-
-run(function()
-    local SigridExploit
-    local Kit, Mount = 'elk_master', bedwars.Client:Get('ElkKitMounted')
-    SigridExploit = vape.Categories.Blatant:CreateModule({
-        Name = 'Infinite Sigrid',
-        Tooltip = 'Lets you ride in the elk forever',
-        Function = function(call)
-            if call then
-                repeat
-                    if entitylib.isAlive then
-                        if store.equippedKit == Kit then
-                            Mount:SendToServer()
-                        end
-                    end
-                    task.wait()
-                until not SigridExploit.Enabled
-            end
-        end
-    })
-end)	
